305caeb7c82e245b81c396f0afec95cc6e2a0e5e
刘建秋 <stakgo@163.com>
2020-10-11 19:35:32 +0800

    初始化

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ api_key=
 api_secret =
 
 # 图片资源输入路径
-tag_input="/Users/jackliu/workspace-python/image_tagging/images"
+tag_input="./image_tagging/images"
 
 # 图片tagged标记输出路径
-tag_output="/Users/jackliu/workspace-python/image_tagging/output"
+tag_output="./image_tagging/output"
 
 # 调试模式
 debug_mode=False
